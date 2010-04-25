0191dc2fffe7cb9567cac1b9782e33c0a0e9e16b
Florian Schlachter <flori@n-schlachter.de>
2010-04-26 01:40:35 +0200

    added expire

--- dpaste/api.py
+++ dpaste/api.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+
 from simpleapi import Namespace
 
 from models import Snippet
 
 class PasteNamespace(Namespace):
     
-    def new(self, content, title='', author=''):
+    def new(self, content, title='', author='', expires=60*60):
         
         new_snippet = Snippet(
             title=title,
             author=author,
-            content=content
+            content=content,
+            expires=datetime.datetime.now() + datetime.timedelta(seconds=expires)
         )
         new_snippet.save()
         
         return new_snippet.get_absolute_url()
     new.published = True
+    new.constraints = {'expires': int}
